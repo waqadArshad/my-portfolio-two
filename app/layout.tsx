db2dae7ff3584dfec9fb36925a8e08adc7d7c50d
Waqad <waqadarshad2@gmail.com>
2025-08-22 04:02:49 +0500

    added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,45 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Waqad - Flutter Developer",
-  description: "Waqad's Portfolio",
+  description: "Portfolio of Waqad Arshad – Senior Flutter Developer",
+  // description: "Waqad's Portfolio",
+  // title: "Waqad Arshad | Flutter Developer Portfolio",
+  // description: "Portfolio of Waqad Arshad – Senior Flutter Developer. Explore my projects, skills, and experience in cross-platform mobile and web app development.",
+  keywords: [
+    "Flutter Developer",
+    "Dart",
+    "React",
+    "Node.js",
+    "Mobile Apps",
+    "Hybrid Apps",
+    "Web Apps",
+    "Cross-platform Development",
+    "Firebase",
+    "Portfolio"
+  ],
+  authors: [{ name: "Waqad Arshad", url: "https://waqadarshad.github.io/portfolio/" }],
+  openGraph: {
+    title: "Waqad Arshad | Flutter Developer Portfolio",
+    description: "Explore my projects, skills, and experience in cross-platform mobile and web app development.",
+    url: "https://waqadarshad.github.io/portfolio/",
+    siteName: "Waqad Arshad Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Waqad Arshad Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Waqad Arshad | Flutter Developer Portfolio",
+    description: "Explore my projects, skills, and experience in cross-platform mobile and web app development.",
+    creator: "@waqad_arshad", // optional
+  },
 };
 
 export default function RootLayout({
