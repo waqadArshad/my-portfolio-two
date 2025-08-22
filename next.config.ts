05d53451ae58e05038b5a7357afa9814b001352e
Waqad <waqadarshad2@gmail.com>
2025-08-22 09:32:06 +0500

    fixed basePath missing a slash

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: 'portfolio', // leave empty for user page, or '/repo-name' for project page
+  basePath: '/portfolio', // leave empty for user page, or '/repo-name' for project page
   assetPrefix: '/portfolio/', // ensures portfolio files load correctly
   images: {
     unoptimized: true, // optional: disables next/image optimization for GH Pages
